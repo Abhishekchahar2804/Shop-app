2e6c1915d07fb37687ebe158827813c5999fadfe
Abhishekchahar2804 <105548044+Abhishekchahar2804@users.noreply.github.com>
2023-06-11 09:09:22 +0530

    adding user and product relationship

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mySql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mySql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-completed',
-    password:'2804As@123'
-})
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
-module.exports = pool.promise();
+module.exports = sequelize;
