c8f60b0bab2fa88c04d2231f16ff46515eb2c6bf
Abhishekchahar2804 <105548044+Abhishekchahar2804@users.noreply.github.com>
2023-07-02 13:47:41 +0530

    relations in mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,10 +14,11 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product({
-    title:title,
-    imageUrl:imageUrl,
-    price:price,
-    description:description
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl,
+    userId: req.user
   });
   product
     .save()
@@ -38,7 +39,6 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Product.findById(prodId)
-    // Product.findById(prodId)
     .then(product => {
       if (!product) {
         return res.redirect('/');
@@ -61,13 +61,13 @@ exports.postEditProduct = (req, res, next) => {
   const updatedDesc = req.body.description;
 
   Product.findById(prodId)
-  .then(product=>{
-    product.title=updatedTitle
-    product.price=updatedPrice
-    product.imageUrl=updatedImageUrl
-    product.description=updatedDesc
-    return product.save()
-  })
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
